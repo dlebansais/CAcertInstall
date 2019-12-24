d9b4f33e6cb7c18b0322c07a2bd3ac1283a78f9f
dlebansais <dlebansais@gmail.com>
2019-12-24 14:44:02 +0100

    Updated to .NET Framework 4.8 and C# 8.0

--- CAcertInstall/Properties/AssemblyInfo.cs
+++ CAcertInstall/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.21")]
-[assembly: AssemblyFileVersion("1.0.0.28")]
+[assembly: AssemblyVersion("1.0.0.35")]
+[assembly: AssemblyFileVersion("1.0.0.42")]
 [assembly: NeutralResourcesLanguage("en-US")]
