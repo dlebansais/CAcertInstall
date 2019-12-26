822a72c32c2a98521958d34928a4957e0e4a238e
dlebansais <dlebansais@gmail.com>
2019-12-26 08:38:18 +0100

    Misc changes.

--- CAcertInstall/Properties/AssemblyInfo.cs
+++ CAcertInstall/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CAcertInstall")]
-[assembly: AssemblyCopyright("Copyright © David Le Bansais 2014")]
+[assembly: AssemblyCopyright("Copyright © David Le Bansais 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.59")]
-[assembly: AssemblyFileVersion("1.0.0.66")]
+[assembly: AssemblyVersion("1.0.0.60")]
+[assembly: AssemblyFileVersion("1.0.0.67")]
 [assembly: NeutralResourcesLanguage("en-US")]
