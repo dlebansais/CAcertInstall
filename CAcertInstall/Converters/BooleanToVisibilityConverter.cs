ef5cc73c95484baa9288fc3950ae17d6c37a6a33
dlebansais <dlebansais@gmail.com>
2019-12-26 08:08:05 +0100

    Misc change.

--- CAcertInstall/Converters/BooleanToVisibilityConverter.cs
+++ CAcertInstall/Converters/BooleanToVisibilityConverter.cs
@@ -12,8 +12,17 @@ namespace Converters
     {
         public object Convert(object value, Type target_type, object parameter, CultureInfo culture)
         {
+            if (!(value is bool))
+                throw new ArgumentOutOfRangeException(nameof(value));
+            if (!(parameter is CompositeCollection))
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+
             bool BooleanValue = (bool)value;
             CompositeCollection VisibilityList = (CompositeCollection)parameter;
+
+            if (VisibilityList.Count < 2)
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+
             return BooleanValue ? VisibilityList[0] : VisibilityList[1];
         }
 
