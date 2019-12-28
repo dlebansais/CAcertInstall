2420cf97bfeeb9c75d138ef42fc819e3438926e3
dlebansais <dlebansais@gmail.com>
2019-12-28 11:21:31 +0100

    Updated the boolean to object converter.

--- CAcertInstall/Converters/BooleanToObjectConverter.cs
+++ CAcertInstall/Converters/BooleanToObjectConverter.cs
@@ -1,51 +1,60 @@
 ﻿namespace Converters
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
-    using System.Windows;
     using System.Windows.Data;
 
     /// <summary>
-    /// Represents a converter from a <see cref="bool"/> instance to a <see cref="Visibility"/> instance.
+    /// Converter from a boolean or nullable boolean to the first or second object of a collection.
     /// </summary>
-    [ValueConversion(typeof(bool), typeof(Visibility))]
-    public class BooleanToObjectConverter : IValueConverter
+    [ValueConversion(typeof(bool), typeof(object))]
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instanciated in Xaml")]
+    internal class BooleanToObjectConverter : IValueConverter
     {
         /// <summary>
-        /// Converts a <see cref="bool"/> value to a <see cref="Visibility"/> instance in a binding.
+        /// Converter from a boolean or nullable boolean to the first or second object of a collection.
         /// </summary>
         /// <param name="value">The value produced by the binding source.</param>
-        /// <param name="target_type">The type of the binding target property.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value.</returns>
-        public object Convert(object value, Type target_type, object parameter, CultureInfo culture)
+        /// <returns>
+        /// If <paramref name="value"/> is a bool and equal to True, or a bool? and also equal to true, and <paramref name="parameter"/> is a collection with at least two objects, the converter returns the second object in the collection.
+        /// Otherwise, if collection has at least two objects, the converter returns the first object in the collection.
+        /// Otherwise, this method throws an exception.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is bool))
+            bool BooleanValue;
+
+            if (value is bool)
+                BooleanValue = (bool)value;
+            else if (value is bool?)
+                BooleanValue = ((bool?)value) ?? false;
+            else
                 throw new ArgumentOutOfRangeException(nameof(value));
-            if (!(parameter is CompositeCollection))
+
+            if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return BooleanValue ? CollectionOfItems[1] : CollectionOfItems[0];
+            else
                 throw new ArgumentOutOfRangeException(nameof(parameter));
-
-            bool BooleanValue = (bool)value;
-            CompositeCollection VisibilityList = (CompositeCollection)parameter;
-
-            if (VisibilityList.Count < 2)
-                throw new ArgumentOutOfRangeException(nameof(parameter));
-
-            return BooleanValue ? VisibilityList[1] : VisibilityList[0];
         }
 
         /// <summary>
-        /// Converts a <see cref="Visibility"/> value to a <see cref="bool"/> instance in a binding.
+        /// Converts an object to a <see cref="bool"/> instance in a binding.
         /// </summary>
         /// <param name="value">The value that is produced by the binding target.</param>
-        /// <param name="target_type">The type to convert to.</param>
+        /// <param name="targetType">The type to convert to.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
         /// <returns>A converted value.</returns>
-        public object ConvertBack(object value, Type target_type, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return value == CollectionOfItems[1];
+            else
+                throw new ArgumentOutOfRangeException(nameof(parameter));
         }
     }
 }
