dc57503ee2b56c2cfe49b1b5a4f944f072c3c2dd
dlebansais <dlebansais@gmail.com>
2020-01-15 21:47:52 +0100

    Cleanup.

--- coverage.bat
+++ coverage.bat
@@ -1,11 +1,12 @@
 @echo off
 
 if not exist ".\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe" goto error1
-if "%WINAPPDRIVER_DIR%" == "" goto error2
-if not exist "%WINAPPDRIVER_DIR%/WinAppDriver.exe" goto error2
-if "%VSTESTPLATFORM_DIR%" == "" goto error3
-if not exist "%VSTESTPLATFORM_DIR%/VSTest.Console.exe" goto error3
-if not exist ".\CAcertInstall\bin\x64\Debug\CAcertInstall.exe" goto error4
+if not exist ".\packages\Codecov.1.9.0\tools\codecov.exe" goto error2
+if "%WINAPPDRIVER_DIR%" == "" goto error3
+if not exist "%WINAPPDRIVER_DIR%/WinAppDriver.exe" goto error3
+if "%VSTESTPLATFORM_DIR%" == "" goto error4
+if not exist "%VSTESTPLATFORM_DIR%/VSTest.Console.exe" goto error4
+if not exist ".\CAcertInstall\bin\x64\Debug\CAcertInstall.exe" goto error5
 
 echo Administor permissions required. Detecting permissions...
 
@@ -13,44 +14,44 @@ net session >nul 2>&1
 if %errorLevel% == 0 (
     echo Success: Administrator permissions confirmed.
 ) else (
-    goto error5
+    goto error6
 )
 
-if exist .\CAcertInstall\obj\x64\Debug\Coverage-CAcertInstall-Debug_coverage.xml del .\CAcertInstall\obj\x64\Debug\Coverage-CAcertInstall-Debug_coverage.xml
+if exist .\CAcertInstall\obj\x64\Debug\Coverage-Debug_coverage.xml del .\CAcertInstall\obj\x64\Debug\Coverage-Debug_coverage.xml
 
 call .\coverage\cleanup.bat
 
-call .\coverage\app.bat Debug "--uninstall"
+call .\coverage\app.bat CAcertInstall Debug "--uninstall"
 call .\coverage\wait.bat 20
-call .\coverage\app_merge.bat Debug "--language=bad"
+call .\coverage\app.bat CAcertInstall Debug "--language=bad"
 call .\coverage\wait.bat 20
-call .\coverage\app_merge.bat Debug "bad"
+call .\coverage\app.bat CAcertInstall Debug "bad"
 call .\coverage\wait.bat 20
 
 start cmd /k .\coverage\start_winappdriver.bat
 
-call .\coverage\app_merge.bat Debug "--language=040C"
+call .\coverage\app.bat CAcertInstall Debug "--language=040C"
 "%VSTESTPLATFORM_DIR%\VSTest.Console.exe" ".\Test-CAcertInstall\bin\x64\Debug\Test-CAcertInstall.dll" /Tests:TestInstall1
 
-call .\coverage\app_merge.bat Debug "--language=0409"
+call .\coverage\app.bat CAcertInstall Debug "--language=0409"
 "%VSTESTPLATFORM_DIR%\VSTest.Console.exe" ".\Test-CAcertInstall\bin\x64\Debug\Test-CAcertInstall.dll" /Tests:TestInstall2
 
-call .\coverage\app_merge.bat Debug "--language=0409"
+call .\coverage\app.bat CAcertInstall Debug "--language=0409"
 "%VSTESTPLATFORM_DIR%\VSTest.Console.exe" ".\Test-CAcertInstall\bin\x64\Debug\Test-CAcertInstall.dll" /Tests:TestInstall3
 
-call .\coverage\app_merge.bat Debug "--language=0409"
+call .\coverage\app.bat CAcertInstall Debug "--language=0409"
 "%VSTESTPLATFORM_DIR%\VSTest.Console.exe" ".\Test-CAcertInstall\bin\x64\Debug\Test-CAcertInstall.dll" /Tests:TestInstall4
 
-call .\coverage\app_merge.bat Debug
+call .\coverage\app.bat CAcertInstall Debug
 call .\coverage\wait.bat 20
-call .\coverage\app_merge.bat Debug "--uninstall --fail=Uninstall"
+call .\coverage\app.bat CAcertInstall Debug "--uninstall --fail=Uninstall"
 call .\coverage\wait.bat 20
-call .\coverage\app_merge.bat Debug "--uninstall"
+call .\coverage\app.bat CAcertInstall Debug "--uninstall"
 call .\coverage\wait.bat 20
-call .\coverage\app_merge.bat Debug "--uninstall --fail=InstalledCheck"
+call .\coverage\app.bat CAcertInstall Debug "--uninstall --fail=InstalledCheck"
 call .\coverage\wait.bat 20
 
-call .\coverage\app_merge.bat Debug "--language=0409 --fail=Install"
+call .\coverage\app.bat CAcertInstall Debug "--language=0409 --fail=Install"
 "%VSTESTPLATFORM_DIR%\VSTest.Console.exe" ".\Test-CAcertInstall\bin\x64\Debug\Test-CAcertInstall.dll" /Tests:TestInstall5
 
 start cmd /c .\coverage\stop_winappdriver.bat
@@ -58,7 +59,7 @@ start cmd /c .\coverage\stop_winappdriver.bat
 call .\coverage\restore.bat
 
 call ..\Certification\set_tokens.bat
-if exist .\CAcertInstall\obj\x64\Debug\Coverage-CAcertInstall-Debug_coverage.xml .\packages\Codecov.1.9.0\tools\codecov -f ".\CAcertInstall\obj\x64\Debug\Coverage-CAcertInstall-Debug_coverage.xml" -t "%CACERTINSTALL_CODECOV_TOKEN%"
+if exist .\CAcertInstall\obj\x64\Debug\Coverage-Debug_coverage.xml .\packages\Codecov.1.9.0\tools\codecov -f ".\CAcertInstall\obj\x64\Debug\Coverage-Debug_coverage.xml" -t "%CACERTINSTALL_CODECOV_TOKEN%"
 goto end
 
 :error1
@@ -66,18 +67,22 @@ echo ERROR: OpenCover.Console not found. Restore it with Nuget.
 goto end
 
 :error2
-echo ERROR: WinAppDriver not found. Example: set WINAPPDRIVER_DIR=C:\Program Files\Windows Application Driver
+echo ERROR: Codecov not found. Restore it with Nuget.
 goto end
 
 :error3
-echo ERROR: Visual Studio 2019 not found. Example: set VSTESTPLATFORM_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\TestPlatform
+echo ERROR: WinAppDriver not found. Example: set WINAPPDRIVER_DIR=C:\Program Files\Windows Application Driver
 goto end
 
 :error4
-echo ERROR: CAcertInstall.dll not built.
+echo ERROR: Visual Studio 2019 not found. Example: set VSTESTPLATFORM_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\TestPlatform
 goto end
 
 :error5
+echo ERROR: CAcertInstall.dll not built.
+goto end
+
+:error6
 echo ERROR: This command line console must be run as administrator.
 goto end
 
--- coverage/app.bat
+++ coverage/app.bat
@@ -1,3 +1,8 @@
 @echo off
-echo Parameter: %2
-start "CAcertInstall" /B ".\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe" -register:Path64 -target:".\CAcertInstall\bin\x64\%1\CAcertInstall.exe" -targetargs:%2 -output:".\CAcertInstall\obj\x64\%1\Coverage-CAcertInstall-%1_coverage.xml"
+echo Application: %1
+echo Parameter: %3
+if not exist ".\%1\obj\x64\%2\Coverage-%2_coverage.xml" goto nomerge
+set MERGE=-mergeoutput
+:nomerge
+start "%1" /B ".\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe" -register:Path64 -target:".\%1\bin\x64\%2\%1.exe" -targetargs:%3 -output:".\%1\obj\x64\%2\Coverage-%2_coverage.xml" %MERGE%
+set MERGE=
