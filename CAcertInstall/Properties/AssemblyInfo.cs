8a1de1121712e360b829993dc961ff3b15efaf70
dlebansais <dlebansais@gmail.com>
2020-10-05 20:07:37 +0200

    Moved dependencies to packages.

--- CAcertInstall/Properties/AssemblyInfo.cs
+++ CAcertInstall/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.0.0.187")]
-[assembly: AssemblyFileVersion("1.0.0.186")]
+[assembly: AssemblyVersion("1.0.0.188")]
+[assembly: AssemblyFileVersion("1.0.0.187")]
