24a021dbe6b568644c0373354b9b52544e3e92e2
dlebansais <dlebansais@gmail.com>
2020-10-06 21:06:07 +0200

    CI updated.

--- Test-CAcertInstall/Properties/AssemblyInfo.cs
+++ Test-CAcertInstall/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("642f685c-9b95-47ca-b290-e2e05fa34424")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.187")]
-[assembly: AssemblyFileVersion("1.0.0.20")]
+[assembly: AssemblyVersion("1.0.1.190")]
+[assembly: AssemblyFileVersion("1.0.0.21")]
 [assembly: NeutralResourcesLanguage("en-US")]
